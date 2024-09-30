f0bba0b281ced7b51da77974db7d5bd9d1073394
ujjwall7 <sharmaujjwal0921@gmail.com>
2024-09-30 08:16:04 +0530

    New update

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\prati\onedrive\desktop\my projects\csv_data_management_with_django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
